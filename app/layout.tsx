8d7117f95ba4594c6c738ca89a9e54afa4c01c03
zyk121381 <124492696+zyk121381@users.noreply.github.com>
2023-11-14 21:47:54 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Revelation's GPT",
+  description: "Revelation's personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
